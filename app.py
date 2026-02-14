cae540731dab72a778a6945c0fd333d17ba036e5
jessecsil <timbeta.jesse@gmail.com>
2026-02-14 03:51:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ if not os.path.exists(ARQUIVO):
   df_vazio.to_csv(ARQUIVO, index=False)
 
 with st.form("form_abastece", clear_on_submit=True):
-data_input = st.date_input("Data", datetime.now())
-v_gnv = st.number_input("GNV (R$)", min_value=0.0)
-v_gas = st.number_input("Gasolina (R$)", min_value=0.0)
-submit = st.form_submit_button("SALVAR REGISTRO")
+  data_input = st.date_input("Data", datetime.now())
+  v_gnv = st.number_input("GNV (R$)", min_value=0.0)
+  v_gas = st.number_input("Gasolina (R$)", min_value=0.0)
+  submit = st.form_submit_button("SALVAR REGISTRO")
 
 st.divider()
 
 if os.path.exists(ARQUIVO):
-df_view = pd.read_csv(ARQUIVO)
-st.dataframe(df_view, use_container_width=True)
+  df_view = pd.read_csv(ARQUIVO)
+  st.dataframe(df_view, use_container_width=True)
