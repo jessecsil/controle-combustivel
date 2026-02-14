c7b45b25a81b1e6a403bee4d7e19c416e23691a0
jessecsil <timbeta.jesse@gmail.com>
2026-02-14 04:11:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,18 @@ import os
 from datetime import datetime
 
 st.set_page_config(page_title="Abastece 2026")
-st.title("Abastece 2026")
+st.title("⛽ Controle de Combustível")
 
 ARQUIVO = "dados.csv"
 
 if not os.path.exists(ARQUIVO):
-  df_vazio = pd.DataFrame(columns=['DATA', 'GNV', 'GAS', 'TOTAL'])
-  df_vazio.to_csv(ARQUIVO, index=False)
+  pd.DataFrame(columns=['DATA', 'GNV', 'GAS', 'TOTAL']).to_csv(ARQUIVO, index=False)
 
-with st.form("form_abastece", clear_on_submit=True):
+with st.form("meu_form", clear_on_submit=True):
   data_input = st.date_input("Data", datetime.now())
-  v_gnv = st.number_input("GNV (R$)", min_value=0.0)
-  v_gas = st.number_input("Gasolina (R$)", min_value=0.0)
-  submit = st.form_submit_button("SALVAR REGISTRO")
+  v_gnv = st.number_input("GNV (R$)")
+  v_gas = st.number_input("Gasolina (R$)")
+  submit = st.form_submit_button("SALVAR")
 
 st.divider()
 
