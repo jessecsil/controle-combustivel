946230615ec6857aa55cf82c372606d3fbf21256
jessecsil <timbeta.jesse@gmail.com>
2026-02-14 03:22:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ DB = 'dados_combustivel.csv'
 
 def carregar_dados():
 if os.path.exists(DB):
+try:
 return pd.read_csv(DB)
+except:
+return pd.DataFrame(columns=['DATA', 'GNV', 'GAS', 'TOTAL'])
 return pd.DataFrame(columns=['DATA', 'GNV', 'GAS', 'TOTAL'])
 
 if 'dados' not in st.session_state:
@@ -18,12 +21,14 @@ st.session_state.dados = carregar_dados()
 
 st.title("⛽ Abastece 2026")
 
-with st.form("meu_form", clear_on_submit=True):
+FORMULÁRIO SIMPLES
+with st.form("meu_form"):
 dt = st.date_input("Data", datetime.now())
 gnv = st.number_input("Valor GNV (R$)", min_value=0.0, format="%.2f")
 gas = st.number_input("Valor Gasolina (R$)", min_value=0.0, format="%.2f")
 salvar = st.form_submit_button("SALVAR AGORA")
 
+EXIBIÇÃO
 df = st.session_state.dados
 if not df.empty:
 st.divider()
