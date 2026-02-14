b41bdfe85c735f9038379af7006497f1c58cea31
jessecsil <timbeta.jesse@gmail.com>
2026-02-14 07:54:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -99,9 +99,9 @@ if os.path.exists(ARQUIVO):
         # Totais menores e organizados em linhas
         st.markdown(f"""
         <div style='font-size:14px;'>
-        <strong>Total GNV R$:</strong> {total_gnv:.2f} <br>
-        <strong>Total Gasolina R$:</strong> {total_gas:.2f} <br>
-        <strong>Total Geral:</strong> R$ {total_geral:.2f}
+        <strong>Total GNV: R$:</strong> {total_gnv:.2f} <br>
+        <strong>Total Gasolina: R$:</strong> {total_gas:.2f} <br>
+        <strong>Total Geral: R$:</strong> {total_geral:.2f}
         </div>
         """, unsafe_allow_html=True)
 
